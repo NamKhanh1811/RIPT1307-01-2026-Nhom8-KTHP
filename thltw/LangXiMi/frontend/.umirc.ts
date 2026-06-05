4bb546bef8b5602c29316318309e98f28a320b5d
Nguyen Duc Hieu <iceminato31@gmail.com>
2026-06-05 13:20:22 +0700

    locale VN

--- thltw/LangXiMi/frontend/.umirc.ts
+++ thltw/LangXiMi/frontend/.umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
-  antd: {},
+  antd: { configProvider: { locale: 'vi_VN' } },
   access: {},
   model: {},
   initialState: {},
