7bc18be82634893b81a076c81ffd7e1e7976a8af
NamKhanh1811 <khanhnn1811@gmail.com>
2026-06-07 10:43:36 +0700

    email netlify fix

--- backend/src/services/email.js
+++ backend/src/services/email.js
@@ -1,9 +1,10 @@
 const nodemailer = require('nodemailer');
 
+const MAIL_PORT = parseInt(process.env.MAIL_PORT || '587');
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: false,
+  port: MAIL_PORT,
+  secure: MAIL_PORT === 465,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
